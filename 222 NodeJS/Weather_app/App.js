d2ef277e0fa7f1a66a23f30d3569bc702d4dc2a8
Martin Cervantes <cervantes.martine@gmail.com>
2022-02-15 21:11:09 -0700

    Add api info

--- 222 NodeJS/Weather_app/App.js
+++ 222 NodeJS/Weather_app/App.js
@@ -1,11 +1,16 @@
-console.log('Starting')
+require('dotenv').config()
+console.log(process.env.API_KEY)
 
-setTimeout(() =>{
-    console.log('Waiting 2 seconds')
-}, 2000)
+const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233'
 
-setTimeout(() =>{
-    console.log('0 seconds')
-}, 0)
-
-console.log('Stopping')
+// console.log('Starting')
+//
+// setTimeout(() => {
+//     console.log('Waiting 2 seconds')
+// }, 2000)
+//
+// setTimeout(() => {
+//     console.log('0 seconds')
+// }, 0)
+//
+// console.log('Stopping')
